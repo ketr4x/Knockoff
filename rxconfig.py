4f794381bc5aecbc7df68976d335579aac8e4717
ketr4x <mail@ketrax.ovh>
2026-03-23 03:36:55 +0100

    Fixes

--- rxconfig.py
+++ rxconfig.py
@@ -2,7 +2,6 @@ import reflex as rx
 
 config = rx.Config(
     app_name="Knockoff",
-    show_built_with_reflex=False,
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
